7dc2c0225e31451670ff1a219948c5da7d9ae94e
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-03-18 19:14:41 -0400

    made changes to user js

--- models/User.js
+++ models/User.js
@@ -3,41 +3,43 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
-    videos: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'video',
+      username: {
+          type: String, unique: true, required: true, trim: true
       },
-    ],
+      email: {
+          type: String, required: true, unique: true, validate: {
+              // Regex that validates email. 
+              validator: function (v) {
+                  return /.+\@.+\..+./.test(v);
+              },
+              message: props => `${props.value} is not a valid email address`
+          },
+      },
+      thoughts: [
+          {
+              type: Schema.Types.ObjectId,
+              ref: 'thought'
+          }
+      ],
+      friends: [
+          {
+              type: Schema.Types.ObjectId,
+              ref: 'user'
+          }
+      ]
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
+      toJSON: {
+          virtuals: true,
+      },
+      id: false,
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
-// Initialize our User model
 const User = model('user', userSchema);
 
 module.exports = User;
