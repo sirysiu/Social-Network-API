f3e84c6cf014e76ff8592e4865b02a40545101a5
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-03-18 19:11:15 -0400

    added user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Thought = require('../models/Thought')
 
 module.exports = {
   async getUsers(req, res) {
@@ -32,4 +33,62 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async updateUser(req, res) {
+    try {
+        const updatedUser = await User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true });
+    if (!updatedUser) {
+        return res.status(404).json({ message: 'No user with that ID! Try Again' })
+    }
+    res.json({ message: 'User successfully updated', updatedUser});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+async deleteUser(req, res) {
+    try {
+        const deletedUser = await User.findOneAndDelete({ _id: req.params.userId });
+    if (!deletedUser) {
+        return res.status(404).json({ message: 'No user with that ID! Try Again' })
+    }
+    // This will delete the thoughts associated with that user. 
+    console.log(deletedUser);
+    await Thought.deleteMany({ _id: {$in:deletedUser.thoughts}});
+    res.json({ message: 'User successfully deleted', deletedUser});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+},
+async addFriend(req, res) {
+    try {
+        const updatedFriend = await User.findOneAndUpdate(
+            { _id: req.params.userId }, 
+            { $push: { friends: req.params.friendId } }, 
+            { new: true });
+    if (!updatedFriend) {
+        return res.status(404).json({ message: 'Unable to add friend! Try Again' })
+    }
+    res.json({ message: 'Friend successfully added', updatedFriend});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+async deleteFriend(req, res) {
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'No user with that ID! Try Again' });
+        }
+
+        res.json({ message: 'Friend successfully removed', updatedUser });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
 };
